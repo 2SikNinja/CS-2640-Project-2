fdd533af8a332154baceb708775b39f9c2992353
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-10-17 03:12:28 -0700

    first commit

--- dates.s
+++ dates.s
@@ -10,17 +10,64 @@
 #
 #
                .data
-promptMonth:   .asciiz        "Please enter the month: "
-promptDay:     .asciiz        "Please enter the day: "
-promptYear:    .asciiz        "Please enter the year: "
+promptMonth:   .asciiz        "Please enter the month: "    #$t3
+promptDay:     .asciiz        "Please enter the day: "      #$t4
+promptYear:    .asciiz        "Please enter the year: "     #$t5
+
 
                .text
-main:          #prints out month prompt
-               li $v0,   4
-               la $a0,   prompt
+main:          
+               li $v0,   4                                  #prints out month prompt
+               la $a0,   promptMonth
                syscall
 
+               #Get the user input for their month
+               li $v0,   5
+               syscall
+
+     move      $t3,      $v0                                #finish month part
+
+               li $v0,   4                                  #prints out day prompt
+               la $a0, promptDay
+               syscall
+               
                #Get the user input for their day
                li $v0,   5
                syscall
+
+     move      $t4,      $v0                                #finish day part
+
+               li $v0,   4                                  #prints out year prompt
+               la $a0, promptYear
+               syscall
                
+               #Get the user input for their day
+               li $v0,   5
+               syscall
+
+     move      $t5,      $v0                                #finish year part
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+End:
+               li $v0,   10
+               syscall
